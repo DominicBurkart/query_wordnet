36b1b206431407e7b49e7ddec76652fa07aa5109
Dominic Burkart <1dominicb@gmail.com>
2018-07-27 01:30:49 -0400

    remove extraneous functions

--- query_wordnet.py
+++ query_wordnet.py
@@ -1,10 +1,12 @@
-from functools import lru_cache
+import os
 
 from nltk.corpus import wordnet as wn
 
 
-def get_ic(ic):
-    raise NotImplementedError
+def get_ic(ic_str):
+    if os.path.exists(ic_str):
+        raise NotImplementedError("Parse into something with a words() function e.g. wn.ic(genesis, False, 0.0)")
+    raise NotImplementedError("Parse into something with a words() function e.g. wn.ic(genesis, False, 0.0)"
 
 
 def syn_matrix(word1, word2, ic=None):
@@ -34,49 +36,6 @@ def syn_matrix(word1, word2, ic=None):
         raise TypeError("Weird type of IC in syn_matrix: " + str(type(ic)))
 
 
-@lru_cache()
-def avg_similarity(word1, word2):
-    l = list(syn_matrix(word1, word2))
-    if len(l) != 0:
-        return sum(l) / len(l)
-    return 0
-
-
-@lru_cache() # this doesn't work in practice.
-def match_similarity(word1, word2, ic=None, symmetric=True):
-    s1s = wn.synsets(word1)
-    s2s = wn.synsets(word2)
-    try:
-        if ic is None:
-            maxes = [max(s1.lch_similarity(s2)
-                         if s1.pos() == s2.pos()
-                            and s1.lch_similarity(s2) is not None
-                         else 0
-                         for s2 in s2s)
-                     for s1 in s1s]
-        elif ic is dict:
-            maxes = [max(s1.res_similarity(s2, ic)
-                         if s1.pos() == s2.pos()
-                            and s1.res_similarity(s2, ic) is not None
-                         else 0
-                         for s2 in s2s)
-                     for s1 in s1s]
-        elif ic is str:
-            maxes = [max(s1.res_similarity(s2, get_ic(ic))
-                         if s1.pos() == s2.pos()
-                            and s1.res_similarity(s2, get_ic(ic)) is not None
-                         else 0
-                         for s2 in s2s)
-                     for s1 in s1s]
-        if symmetric:
-            return sum((sum(maxes) / len(maxes), match_similarity(word2, word1, ic, symmetric=False))) / 2
-        else:
-            return sum(maxes) / len(maxes)
-    except ValueError or ZeroDivisionError:
-        return 0
-
-
-@lru_cache()
 def max_similarity(word1, word2):
     try:
         return max(syn_matrix(word1, word2))
