2dcff2011e4b2a7dad16fe57434dc9d597c42145
Dominic Burkart <1dominicb@gmail.com>
2018-07-27 13:41:35 -0400

    make IC from nltk or tweets

--- query_wordnet.py
+++ query_wordnet.py
@@ -1,13 +1,47 @@
-import sys
 import os
+import sys
+from functools import lru_cache
 
 from nltk.corpus import wordnet as wn
 
 
 def get_ic(ic_str):
+    @lru_cache(maxsize=1)
+    def into_corpus(dirstr):
+        import pandas as pd
+        from nltk.tokenize import word_tokenize
+        from nltk.corpus.reader.api import concat
+
+        def files(s):
+            for dir, dirs, files in os.walk(s):
+                for f in files:
+                    yield os.path.join(dir, f)
+
+        def chunks(fp):
+            if fp.endswith(".txt"):
+                print("reading in text file as document: " + fp)
+                with open(fp) as f:
+                    return [f.read()]
+            elif fp.endswith(".csv.gz"):
+                print("reading in file as tweet documents: " + fp)
+                return pd.read_csv(fp, compression="gzip").message.values
+
+        return concat(word_tokenize(c) for f in files(dirstr) for c in chunks(f))
+
     if os.path.exists(ic_str):
-        raise NotImplementedError("Parse into something with a words() function e.g. wn.ic(genesis, False, 0.0)")
-    raise NotImplementedError("Parse into something with a words() function e.g. wn.ic(genesis, False, 0.0)"
+        if os.path.isdir(ic_str):
+            print("Assuming path leads to EITHER txt or twitter csv.gz files")
+            return into_corpus(ic_str)
+        elif ic_str == "brown":
+            from nltk.corpus import brown
+            return brown
+        elif ic_str == "web":
+            from nltk.corpus import webtext
+            return webtext
+        elif ic_str.endswith(".dat"):  # assume this is a wordnet corpus.
+            return wn.WordNetICCorpusReader(ic_str)
+        else:
+            raise NotImplementedError
 
 
 def syn_matrix(word1, word2, ic=None):
@@ -15,7 +49,7 @@ def syn_matrix(word1, word2, ic=None):
         for s1 in wn.synsets(word1):
             for s2 in wn.synsets(word2):
                 if s1.pos() == s2.pos():
-                    v = s1.lch_similarity(s2)
+                    v = s1.path_similarity(s2)
                     if v is not None:
                         yield v
 
@@ -37,11 +71,12 @@ def syn_matrix(word1, word2, ic=None):
         raise TypeError("Weird type of IC in syn_matrix: " + str(type(ic)))
 
 
-def max_similarity(word1, word2):
+def max_similarity(word1, word2, ic=None):
     try:
-        return max(syn_matrix(word1, word2))
+        return max(syn_matrix(word1, word2, ic))
     except ValueError:
         return 0  # syn_matrix couldn't be generated.
+
 
 if __name__ == "__main__":
     print(locals()[sys.argv[1]](*sys.argv[2:]))
